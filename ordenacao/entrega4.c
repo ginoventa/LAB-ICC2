1be7cd7d6929c3a898e633712ccae66a58d7d8cf
Ginoventa <gn.noventa@gmail.com>
2025-09-28 13:35:56 -0300

    new: exercício 4 + pdf

---
+++ ordenacao/entrega4.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <stdbool.h>    
+
+typedef enum{
+    amarelo,
+    azul,
+    branco,
+    preto,
+    rosa,
+    verde,
+    vermelho
+} cores;
+
+typedef struct brinquedo{
+    cores cor;
+    float comprimento;
+    float nota;
+    int pos_original;
+} brinquedo;
+
+cores string_to_cor(char *cor_str){
+    if (strcmp(cor_str, "amarelo") == 0) return amarelo;
+    if (strcmp(cor_str, "azul") == 0) return azul;
+    if (strcmp(cor_str, "branco") == 0) return branco;
+    if (strcmp(cor_str, "preto") == 0) return preto;
+    if (strcmp(cor_str, "rosa") == 0) return rosa;
+    if (strcmp(cor_str, "verde") == 0) return verde;
+    if (strcmp(cor_str, "vermelho") == 0) return vermelho;
+    return -1; // valor inválido
+}
+
+bool compara_brinquedos(brinquedo a, brinquedo b){ // verifica se a deve vir antes de b
+    if (a.cor != b.cor) return a.cor < b.cor;
+    if (a.comprimento != b.comprimento) return a.comprimento < b.comprimento;
+    if (a.nota != b.nota) return a.nota > b.nota;
+    return a.pos_original < b.pos_original;
+}
+
+void insertion_sort(brinquedo *a, int n){
+    brinquedo b;
+    int j = 0;
+    for(int i = 1; i < n; i++){
+        b = a[i];
+        j = i;
+        while(j > 0 && compara_brinquedos(b,a[j-1])){
+            a[j] = a[j-1];
+            j = j - 1;
+        }
+        a[j] = b;
+    }
+}
+
+void bubble_sort(brinquedo *brinquedos, int n){
+    for(int i = 0;i < n-1;i++){
+        for (int j = 0;j<n-i-1;j++){
+            if(!compara_brinquedos(brinquedos[j], brinquedos[j+1])){
+                brinquedo temp = brinquedos[j];
+                brinquedos[j] = brinquedos[j+1];
+                brinquedos[j+1] = temp;
+            }
+        }
+    }
+}
+void merge(brinquedo *brinquedos, int left, int mid, int right, brinquedo *aux){
+    int i = left, j = mid + 1, k = left;
+    
+    while(i <= mid && j <= right){
+        if(compara_brinquedos(brinquedos[i], brinquedos[j])){
+            aux[k] = brinquedos[i];
+            i++;
+        }
+        else{
+            aux[k] = brinquedos[j];
+            j++;
+        }
+        k = k + 1;
+    }
+    while(i <= mid){
+        aux[k] = brinquedos[i];
+        i++;
+        k++;
+    }
+
+    while(j <= right){
+        aux[k] = brinquedos[j];
+        j++;
+        k++;
+    }
+}
+
+void mpass(brinquedo* brinquedos, int n, int pairSize, brinquedo* aux){
+    int i = 0;
+    while(i <= n - 2*pairSize){
+        merge(brinquedos, i, i+pairSize-1, i+2*pairSize-1, aux);
+        i = i + 2*pairSize;
+    }
+    if(i + pairSize - 1 < n){
+        merge(brinquedos, i, i+pairSize-1, n-1, aux);
+    }
+    else{
+        for(int j = i; j < n; j++){
+            aux[j] = brinquedos[j];
+        }
+    }
+}
+void merge_sort(brinquedo *brinquedos, int n){
+    int pairSize = 1;
+    brinquedo aux[n]; 
+    while( pairSize < n ){
+        mpass(brinquedos, n, pairSize, aux);
+        pairSize = 2*pairSize;
+        mpass(aux, n, pairSize, brinquedos);
+        pairSize = 2*pairSize;
+    }
+}
+
+void qsort(brinquedo vetor[], int l, int r) {
+    int i, j;
+    brinquedo aux, x;
+    i = l;
+    j = r;
+    x = vetor[(l + r) / 2];
+    do {
+        while (compara_brinquedos(vetor[i], x)) {
+            i++;
+        }
+        while (compara_brinquedos(x, vetor[j])) {
+            j--;
+        }
+        if (i <= j) {
+            aux = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = aux;
+            i++;
+            j--;
+        }
+    } while (i <= j);
+    
+    if (l < j) {
+        qsort_recursive(vetor, l, j);
+    }
+    if (i < r) {
+        qsort_recursive(vetor, i, r);
+    }
+}
+
+void quick_sort(brinquedo *brinquedos, int n) {
+    qsort(brinquedos, 0, n - 1);
+}
+
+void brinquedos_sort(brinquedo *brinquedos, int n, int sel){
+    // Implementação do algoritmo de ordenação (Bubble Sort, Insertion Sort, Merge Sort, Quick Sort)
+    switch (sel){
+        case 1:
+            // Bubble Sort
+            bubble_sort(brinquedos, n);
+            break;
+        case 2:
+            // Insertion Sort
+            insertion_sort(brinquedos, n);
+            break;
+        case 3:
+            // Merge Sort
+            merge_sort(brinquedos, n);
+            break;
+        case 4:
+            // Quick Sort
+            quick_sort(brinquedos, n);
+            break;
+        default:
+            printf("Seleção inválida\n");
+            break;
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    brinquedo brinquedos[n];
+    for (int i = 0; i < n; i++){
+        char cor_str[20]; // buffer temporário para a string da cor
+        scanf("%s %f %f", cor_str, &brinquedos[i].comprimento, &brinquedos[i].nota);
+        brinquedos[i].cor = string_to_cor(cor_str);
+        brinquedos[i].pos_original = i;
+    }
+    int sel = 1;
+    brinquedos_sort(brinquedos, n, sel);
+
+    for(int i = 0; i < n; i++){
+        printf("%d;", brinquedos[i].pos_original);
+    }
+    return 0;
+}
--- volta_usp/volta_usp_bubblesort.c
+++ volta_usp/volta_usp_bubblesort.c
@@ -1,84 +1,78 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdbool.h>
 
-void ordenacao(int *tamanho_nomes, int tamanho, int *comparacoes, int *trocas){
-    int inicio = 0, fim = tamanho - 1;
-    *trocas = 0;
-    *comparacoes = 0;
-    bool troca = true;
-    while(troca){
-        troca = false;
-        for(int n = inicio; n < fim; n++ ){
-            (*comparacoes)++;
-            if (tamanho_nomes[n] > tamanho_nomes[n + 1]) {
-                int aux = tamanho_nomes[n];
-                tamanho_nomes[n] = tamanho_nomes[n+1];
-                tamanho_nomes[n+1] = aux;
-                troca = true;
+void ordenacaoBubblesort(char *vetor, int tamanhoVetor, int *comp, int *trocas)
+{
+    for (int i = 0; i < tamanhoVetor - 1; i++)
+    {
+        for (int j = 0; j < tamanhoVetor - 1 - i; j++)
+        {
+            (*comp)++;
+            if (vetor[j] > vetor[j + 1])
+            {
+                int temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
                 (*trocas)++;
             }
         }
-        fim--;
-        if (!troca) break;
-        troca = false;
-        for (int n = fim; n > inicio; n--) {
-            (*comparacoes)++;
-            if (tamanho_nomes[n] < tamanho_nomes[n - 1]) {
-                int aux = tamanho_nomes[n];
-                tamanho_nomes[n] = tamanho_nomes[n - 1];
-                tamanho_nomes[n - 1] = aux;
-                troca = true;
-                (*trocas)++;
-            }
-        }
-        inicio++;
     }
 }
-
-int contador_palavras(char *str){
-    int nome = 0, i = 0; 
-    while(str[i] != '-' && str[i] != '\0'){
-        if(isalpha(str[i])){
-            nome++; 
+int contador_palavras(char *str)
+{
+    int nome = 0, i = 0;
+    while (str[i] != '-' && str[i] != '\0')
+    {
+        if (isalpha(str[i]))
+        {
+            nome++;
         }
-        i++; 
+        i++;
     }
     return nome;
-}   
-void imprimir(const char* grupo, int tamanho, int* tamanho_nomes, int comparacoes, int trocas){
-    printf("%s - [", grupo);
-    for(int i = 0; i < tamanho; i++) {
-        printf("%d%s", tamanho_nomes[i], (i < tamanho-1) ? ", " : "");
+}
+void imprimir(int tamanho, char *tamanho_nomes, int *comparacoes, int *trocas)
+{
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("%d%s", tamanho_nomes[i], (i < tamanho - 1) ? ", " : "");
     }
     printf("]\n");
-    printf("Comparações: %d, Trocas: %d\n\n", comparacoes, trocas);
+    printf("Comparações: %d, Trocas: %d", *comparacoes, *trocas);
 }
 
-int main(){
-    int nu = 0, ne = 0;  
-    int USP[200], EXTERNA[200]; 
-    char str[200]; 
+int main()
+{
+    int nu = 0, ne = 0;
+    char USP[75000], EXTERNA[75000];
+    char str[120];
 
-    while(fgets(str,200,stdin)){
+    while (fgets(str, 120, stdin))
+    {
         int numero = contador_palavras(str);
-        if(str[strlen(str)-2]=='p' || str[strlen(str)-2]=='P'){
-            USP[nu] = numero; 
-            nu++; 
-        }else{
-            EXTERNA[ne] = numero; 
-            ne++; 
+        if (tolower(str[strlen(str) - 2]) == 'p' || tolower(str[strlen(str) - 1]) == 'p')
+        {
+            USP[nu] = numero;
+            nu++;
+        }
+        else
+        {
+            EXTERNA[ne] = numero;
+            ne++;
         }
     }
-    int comp_usp = 0, troc_usp = 0;
-    int comp_ext = 0, troc_ext = 0;
-    ordenacao(USP, nu, &comp_usp, &troc_usp);
-    imprimir("USP", nu, USP, comp_usp, troc_usp);
-    ordenacao(EXTERNA, ne, &comp_ext, &troc_ext);
-    imprimir("Externa", ne, EXTERNA, comp_ext, troc_ext);
-    return 0; 
+    int comp = 0, troca = 0;
+    ordenacaoBubblesort(USP, nu, &comp, &troca);
+    printf("USP - [");
+    imprimir(nu, USP, &comp, &troca);
+    printf("\n\n");
+    comp = 0, troca = 0;
+    printf("Externa - [");
+    ordenacaoBubblesort(EXTERNA, ne, &comp, &troca);
+    imprimir(ne, EXTERNA, &comp, &troca);
+    return 0;
 }
-
 
 // Etapas: Pegar nomes --> contar caracteres --> salvar quantidade em um vetor
